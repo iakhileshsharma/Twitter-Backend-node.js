770343d47230a3a3826f352453f20a1e5719c041
Akhilesh Sharma <akhileshsharma0217@gmail.com>
2023-12-04 19:58:02 +0530

    Prisma schema and User CRUD operations

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,86 @@
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const router = Router();
+const prisma = new PrismaClient();
+
+// User CRUD
+
+  // Create user
+  router.post('/', async(req, res)=>{
+    const { email, name, username } = req.body;
+
+
+    try{
+      const result = await prisma.user.create({
+        data: {
+          email,
+          name,
+          username,
+          bio: "Hello, I am new on twitter",
+        },
+     });
+ 
+     res.json(result);
+    } catch(error){
+      res.status(400).json({ error: "Username and email should be unique"});
+    }
+  
+  });
+
+
+
+
+  // List users
+  router.get('/', async(req, res)=>{
+    const allUser = await prisma.user.findMany();
+
+    res.json(allUser);
+  });
+
+
+
+
+  // get one user
+  router.get('/:id', async(req, res)=>{
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id: Number(id) }});
+
+
+    res.json(user);
+  });
+
+
+
+
+  // update user
+  router.put('/:id', async(req, res)=>{
+    const { id } = req.params;
+
+    const { bio, name, image } = req.body;
+
+    try{
+      const result = await prisma.user.update({
+        where: { id: Number(id)},
+        data: { bio, name, image }
+      })
+      res.json(result);
+    } catch(error){
+      res.status(400).json({error: 'Failed to update the user'});
+    }
+  });
+
+
+
+
+
+ // delete user
+ router.delete('/:id', async (req, res)=>{
+  const { id } = req.params;
+  const result = await prisma.user.delete({where: {id: Number(id)}});
+  res.json(result);
+ });
+
+
+
+export default router;
